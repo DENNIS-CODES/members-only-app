356cb1ab4fab8b94ceaf52fe5734cc25e4ca8ded
Dennis Mwangi <dennistrevor06@gmail.com>
2021-02-01 13:32:00 +0300

    server

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const app =  express();
+
+app.get('/', (req, res) => {
+    res.send('server is ready');
+});
+app.listen(500, () => {
+    console.log('server at http://localhost:5000');
+});
